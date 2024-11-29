17a6ef6664488658bf1117f2c0dd58bdedff9320
sreenanda <sreenandakanad@gmail.com>
2024-11-29 15:15:25 +0530

    29 november aft

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project1/admin/admin_login.dart';
+import 'package:project1/firebase_options.dart';
+import 'package:project1/user/forgot_password_page.dart';
 import 'package:project1/user/home_page.dart';
 import 'package:project1/user/login_page.dart';
 import 'package:project1/user/registration%20screen.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: LoginPage(),
